6406f6524cfd1af3d9390103b6e0373899bd8206
Xin Tong <xint5@illinois.edu>
2020-10-19 17:17:30 -0500

    Add Main class to execute parser and store values in pool

---
+++ src/main/java/com/github/tongxin97/Main.java
@@ -0,0 +1,9 @@
+package com.github.tongxin97;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    MethodParser methodParser = new MethodParser(args[0]);
+    MethodPool methodPool = new MethodPool();
+    methodParser.CollectMethodInfo(methodPool);
+  }
+}
