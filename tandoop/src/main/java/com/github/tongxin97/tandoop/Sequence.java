d442b062bf6399a9324103c94c442c27dd0012f2
tangrizzly <tangyyuanr@gmail.com>
2020-10-21 15:04:02 -0500

    Changed runTest

--- tandoop/src/main/java/com/github/tongxin97/tandoop/Sequence.java
+++ tandoop/src/main/java/com/github/tongxin97/tandoop/Sequence.java
@@ -19,34 +19,29 @@ public class Sequence {
     public Sequence() {}
 
     public void generateTest() throws Exception {
-        String sequence = "package com.github.tongxin97.tandoop;\n"
-        		+ "\n"
-        		+ "import static org.junit.Assert.assertTrue;\n"
-        		+ "\n"
+        String sequence = "import static org.junit.Assert.assertEquals;\n"
         		+ "import org.junit.Test;\n"
         		+ "\n"
-        		+ "/**\n"
-        		+ " * Unit test for simple App.\n"
-        		+ " */\n"
-        		+ "public class AppTest \n"
-        		+ "{\n"
-        		+ "    /**\n"
-        		+ "     * Rigorous Test :-)\n"
-        		+ "     */\n"
-        		+ "    @Test\n"
-        		+ "    public void shouldAnswerWithTrue()\n"
-        		+ "    {\n"
-        		+ "        assertTrue( true );\n"
-				+ "    }\n"
-        		+ "    @Test\n"
-        		+ "    public void shouldAnswerWithFalse()\n"
-        		+ "    {\n"
-        		+ "        assertTrue( !false );\n"
-        		+ "    }\n"
-        		+ "}\n";
-        String filename = "../app/src/test/java/com/github/tongxin97/app/AppTest.java";
+        		+ "public class CalculatorTest {\n"
+        		+ "  @Test\n"
+        		+ "  public void evaluatesExpression() {\n"
+        		+ "    Calculator calculator = new Calculator();\n"
+        		+ "    int sum = calculator.evaluate(\"1+2+3\");\n"
+        		+ "    assertEquals(6, sum);\n"
+        		+ "  }\n"
+        		+ "}";
+        String filename = "../calculator/CalculatorTest.class";
         BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
         writer.write(sequence);
         writer.close();
-    }
+	}
+	
+	public void runTest() {
+		/*
+		cd ../calculator
+		javac Calculator.java
+		javac -cp .:junit-4.13.1.jar:hamcrest-core-1.3.jar CalculatorTest.java
+		java -cp .:junit-4.13.1.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore CalculatorTest
+		*/
+	}
 }
