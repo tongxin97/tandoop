0bb83b35013665f5baf7effe64ab4671890b86ab
tangrizzly <tangyyuanr@gmail.com>
2020-10-25 15:15:50 -0500

    Use commons-cli to parse arguments

--- tandoop/src/main/java/com/github/tongxin97/tandoop/Main.java
+++ tandoop/src/main/java/com/github/tongxin97/tandoop/Main.java
@@ -1,9 +1,34 @@
 package com.github.tongxin97.tandoop;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.Options;
+
 public class Main {
   public static void main(String[] args) throws Exception {
-    Tandoop tandoop = new Tandoop(args[0]);
+    CommandLineParser parser = new DefaultParser();
+    Options options = new Options();
+    options.addOption("pkg", "pkgName", false, "Package name");
+    options.addOption("src", "srcDir", false, "Project src directory");
+    options.addOption("test", "testDir", false, "Project test directory");
 
-    tandoop.generateSequence(3);
+    try {
+      // parse cmdline arguments
+      CommandLine cmd = parser.parse(options, args);
+      if (!cmd.hasOption("pkg")) {
+        System.err.println("Package name not provided.");
+      }
+      // args: String testDir, String pkgName
+      Tandoop tandoop = new Tandoop(
+        cmd.getOptionValue("test"), 
+        cmd.getOptionValue("pkg")
+      );
+      tandoop.generateSequence(20);
+
+    } catch (ParseException e) {
+      System.err.println( "Unexpected exception:" + e.getMessage() );
+    }
   }
 }
