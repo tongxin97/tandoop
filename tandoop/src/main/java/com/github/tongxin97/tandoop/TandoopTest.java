0db1d700dd486f7c828297de4b648d135ae8501b
Xin Tong <xint5@illinois.edu>
2020-11-06 13:41:01 -0600

    Added placeholder test

---
+++ tandoop/src/main/java/com/github/tongxin97/tandoop/TandoopTest.java
@@ -0,0 +1,13 @@
+package com.github.tongxin97.tandoop;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.io.*;
+import com.google.gson.Gson;
+
+public class TandoopTest {
+  @Test
+  public void test() {
+    System.out.println("Placeholder Test");
+  }
+}
