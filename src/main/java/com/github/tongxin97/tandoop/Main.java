a39774c0719b4cba02d555c4d3f1a5b4c34ac993
Xin T <placeholder@example.com>
2020-12-11 18:21:47 -0600

    Update cmd flag conditions

--- src/main/java/com/github/tongxin97/tandoop/Main.java
+++ src/main/java/com/github/tongxin97/tandoop/Main.java
@@ -17,9 +17,9 @@ public class Main {
     options.addOption("limit", "timeLimit", true, "Time limit to run Tandoop for in seconds");
     options.addOption("basic", "basicFeatures", false, "If Tandoop should use all basic features.");
     options.addOption("all", "allFeatures", false, "If Tandoop should use all features.");
-    options.addOption("gt", "genericTypes", false, "If Tandoop should allow for generics types in generated test classes.");
-    options.addOption("mi", "methodInheritance", false, "If Tandoop should use method inheritance.");
-    options.addOption("ci", "classInheritance", false, "If Tandoop should use class inheritance.");
+    options.addOption("noGI", "noGenericTypes", false, "If Tandoop should allow for generics types in generated test classes.");
+    options.addOption("noMI", "noMethodInheritance", false, "If Tandoop should use method inheritance.");
+    options.addOption("noCI", "noClassInheritance", false, "If Tandoop should use class inheritance.");
     options.addOption("cg", "coverageGuided", false, "If Tandoop uses coverage-guided method selection.");
     options.addOption("cs", "constructorSelection", false, "If Tandoop uses constructor selection preference.");
     options.addOption("odc", "onDemandConstruction", false, "If Tandoop uses on-demand construction of external types.");
@@ -77,27 +77,13 @@ public class Main {
       testOutputDir.mkdir();
 
       Tandoop tandoop = new Tandoop(cmd.getOptionValue("src"), prjDir);
-      if (cmd.hasOption("all")) {
-        tandoop.allowGenerics = true;
-        tandoop.useMethodInheritance = true;
-        tandoop.useClassInheritance = true;
-        tandoop.useCovGuide = true;
-        tandoop.useConstructorSelection = true;
-        tandoop.useODConstruction = true;
-      } else {
-        if (cmd.hasOption("basic")) {
-          tandoop.allowGenerics = true;
-          tandoop.useMethodInheritance = true;
-          tandoop.useClassInheritance = true;
-        } else {
-          tandoop.allowGenerics = cmd.hasOption("gt");
-          tandoop.useMethodInheritance = cmd.hasOption("mi");
-          tandoop.useClassInheritance = cmd.hasOption("ci");
-        }
-        tandoop.useCovGuide = cmd.hasOption("cg");
-        tandoop.useConstructorSelection = cmd.hasOption("cs");
-        tandoop.useODConstruction = cmd.hasOption("odc");
-      }
+
+      tandoop.allowGenerics = cmd.hasOption("basic") || !cmd.hasOption("noGI");
+      tandoop.useMethodInheritance = cmd.hasOption("basic") || !cmd.hasOption("noMI");
+      tandoop.useClassInheritance = cmd.hasOption("basic") || !cmd.hasOption("noCI");
+      tandoop.useCovGuide = cmd.hasOption("all") || cmd.hasOption("cg");
+      tandoop.useConstructorSelection = cmd.hasOption("all") || cmd.hasOption("cs");
+      tandoop.useODConstruction = cmd.hasOption("all") || cmd.hasOption("odc");
 
       int timeLimit = Integer.parseInt(cmd.getOptionValue("limit"));
       tandoop.generateSequences(timeLimit);
